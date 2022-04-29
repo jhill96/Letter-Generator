f9e6a6b0a71f69b6c5c7c3bc8eed3f505787f598
jhill96 <100241807+jhill96@users.noreply.github.com>
2022-04-29 09:19:41 -0700

    Add files via upload

---
+++ LetterGenerator.java
@@ -0,0 +1,92 @@
+// programmer:  Jonathan Hill
+// 3/11/22
+// Assignment 1
+// Purpose:  The purpose of this program is to create a crossword puzzle.
+// The program will ask you to create a "word generator" by selecting g
+// If you try to produce a word generator without entering in strings then
+// the program will ask you to do that first
+
+// Note:  Sometimes the program gets hung up after entering the 5th string
+// I believe this is because in the generate method at the for while loop,
+// which deals with the fifth string, the program can't exit the while loop.  
+// I haven't had time to debug this. If this happens start the program over.
+
+// Import scanner class
+import java.util.*;
+
+// Start of the LetterGenerator class
+public class LetterGenerator {
+   public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      String userinput = "";
+      WordSearch puzzle = new WordSearch();
+      // create different arrays that I can use in different methods
+      // so I'm not over writing arrays.
+      char [][] a = new char [30][30];
+      char [][] b = new char [30][30];
+      char [][] c = new char [30][30];
+      // start of do/while 
+      do{
+         input = new Scanner(System.in);
+         printIntro();
+         userinput = input.next();
+         if (userinput.equalsIgnoreCase("g")) {
+            a  =  puzzle.generate();
+         } else if (userinput.equalsIgnoreCase("p")) {
+            int sum = 0;
+            for( int i = 0; i<30; i++) {
+               for(int  j = 0; j < 30; j++) {
+            //iterates though the grid array in wordsearch class
+            //and copies information to c array which is passed
+            //to the print() in word search class
+                  c [j][i] = a[j][i];
+            //checks to see if there's any character in the array
+            //by adding up ascii values and comparing to zero
+                  sum = sum + a[j][i];
+               }
+            }
+            if( sum > 0) {
+               puzzle.print(c);
+            } else {
+               System.out.println("Please create a word generator first\n");
+            }
+         } else if (userinput.equalsIgnoreCase("s")) {
+           
+            int sum = 0;
+            for(int  i = 0; i<30; i++) {
+               for(int j = 0; j < 30; j++) {
+            //iterates though the grid array in wordsearch class
+            //and copies information to b array which is passed
+            //to the print() in word search class
+                  b [j][i] = a[j][i];
+            //checks to see if there's any character in the array
+            //by adding up ascii values and comparing to zero
+                  sum = sum + a[j][i];
+               }
+            }
+            if(sum > 0) {
+               puzzle.printSolution(b);
+            } else {
+               System.out.println("Please create a word generator first\n");
+            }
+         } else if (userinput.equalsIgnoreCase("q")) {
+            break;
+         } else {
+            System.out.println("Please enter a valid command");
+         }
+      
+      } while (userinput.equalsIgnoreCase("g") || userinput.equalsIgnoreCase("p") || userinput.equalsIgnoreCase("s")
+            || !userinput.equalsIgnoreCase("g") && !userinput.equalsIgnoreCase("q")|| !userinput.equalsIgnoreCase("p") 
+            && !userinput.equalsIgnoreCase("q")|| !userinput.equalsIgnoreCase("s") && !userinput.equalsIgnoreCase("q"));
+   } // end of do/while 
+        
+   // Start of printIntro method - presents the main menu
+   public static void printIntro() {
+      System.out.printf("%s%n" , "Welcome to my word search generator!");
+      System.out.printf("%s%n" , "Please select an option:");
+      System.out.printf("%s%n" , "Generate a new word search  (enter g)");
+      System.out.printf("%s%n" , "Print out your word search (enter p)");
+      System.out.printf("%s%n" , "Show the solution to your word search (enter s)");
+      System.out.printf("%s%n" , "Quit program (enter q)");
+   } // end of the printIntro method
+} // end of LetterGenerator class
---
+++ WordSearch.java
@@ -0,0 +1,173 @@
+// programmer:  Jonathan Hill
+// 3/11/22
+// Assignment 1
+// Purpose:  The purpose of this program is to create a crossword puzzle.
+// The program will ask you to create a "word generator" by selecting g
+// If you try to produce a word generator without entering in strings then
+// the program will ask you to do that first
+
+// Note:  Sometimes the program gets hung up after entering the 5th string
+// I believe this is because in the generate method at the four while loop,
+// which deals with the fifth string, the program can't exit the while loop.  
+// I haven't had time to debug this. If this happens start the program
+
+// Import scanner class
+import java.util.*;
+
+// Start of WordSeach Class
+public class WordSearch {
+  
+// Start of generate method
+// returns the grid array to the LetterGenrator Class
+// runs the bulk of the program
+  public char [][] generate() {            
+      int height = 30;
+      int length = 30;
+      int range = 20;
+      char [][] grid = new char[length][height];
+      Random rand = new Random();
+      Scanner input = new Scanner(System.in);
+      // generate random numbers that can be used
+      // to place the users strings into the grid array
+      int randomNumber = rand.nextInt(range);
+      int randomNumber2 = rand.nextInt(range);
+      int randomNumber3 = rand.nextInt(range);
+      int randomNumber4 = rand.nextInt(range);
+      int randomNumber5 = rand.nextInt(range);
+      // ask user to enter strings
+      System.out.println("Please enter 5 words");
+      System.out.println("Enter 1st word, 8 characters or less: ");
+      String wordOne = input.next();
+      System.out.println("Enter 2nd word, 8 characters or less: ");
+      String wordTwo = input.next();
+      System.out.println("Enter 3rd word, 8 characters or less: ");
+      String wordThree = input.next();
+      System.out.println("Enter 4th word, 8 characters or less: ");
+      String wordFour= input.next();
+      System.out.println("Enter 5th word, 8 characters or less: ");
+      String wordFive= input.next();
+      // covert strings to uppercase
+      String s = wordOne.toUpperCase();
+      String t = wordTwo.toUpperCase();
+      String q = wordThree.toUpperCase();
+      String u = wordFour.toUpperCase();
+      String v = wordFive.toUpperCase();
+     // for loops to place the strings as individual characters in arrays
+     // the while loops make the computer pick a differnent random number for 
+     // each string to keep the strings from over writing each other
+     for(int i = 0; i < s.length(); i++) {
+         grid [randomNumber + i][randomNumber] = s.charAt(i);
+      }
+      for(int i = 0; i < t.length(); i++) {
+         while(randomNumber2 == randomNumber  || randomNumber2 == randomNumber + 1  
+               || randomNumber2 == randomNumber -1) {
+            randomNumber2 = rand.nextInt(range);
+         }
+         grid [randomNumber + 2][randomNumber2 + i] = t.charAt(i);
+      }
+      for(int i = 0; i < q.length(); i++) {
+         while(randomNumber3 == randomNumber || randomNumber3 == randomNumber2 
+               || randomNumber3 == randomNumber + 1 || randomNumber3 == randomNumber2 + 1 
+               || randomNumber3 == randomNumber - 1 || randomNumber3 == randomNumber2 - 1) {
+            randomNumber3 = rand.nextInt(range);
+         }
+         grid [randomNumber + 4 + i][randomNumber + 4 + i] = q.charAt(i);
+      }
+      for(int i = 0; i < u.length(); i++) {
+         while(randomNumber4 == randomNumber || randomNumber4 == randomNumber2 
+               || randomNumber4 == randomNumber3|| randomNumber4 == randomNumber + 1 
+               || randomNumber4 == randomNumber2 + 1 || randomNumber4 == randomNumber3 + 1
+               || randomNumber4 == randomNumber -1 || randomNumber4 == randomNumber2 -1 
+               || randomNumber4 == randomNumber3 - 1) {
+                   
+            randomNumber4 = rand.nextInt(range);
+         }
+         grid [randomNumber4][randomNumber4 + i] = u.charAt(i);
+      }
+      for(int i = 0; i < v.length(); i++) {
+         // I believe I have a bug in this while loop that
+         // keeps the program occasionally from exiting this loop
+         while(randomNumber5 == randomNumber || randomNumber5 == randomNumber2 
+               || randomNumber5 == randomNumber3 || randomNumber5 == randomNumber4 
+               || randomNumber5 == randomNumber + 1 || randomNumber5 == randomNumber2 + 1 
+               || randomNumber5 == randomNumber3 + 1 || randomNumber2 == randomNumber4 + 1 
+               || randomNumber5 == randomNumber -1 || randomNumber5 == randomNumber2 -1 || 
+               randomNumber5 == randomNumber3 -1 || randomNumber5 == randomNumber4 - 1) {
+                 
+            randomNumber5 = rand.nextInt(range);
+         }
+         grid [randomNumber5 + i][randomNumber5] = v.charAt(i);
+      }
+      return grid;
+      
+   } // end of generate method
+    
+     // Start of print method
+     // prints out cross word puzzle
+     public void print(char [][] c) {
+      int height = 30;
+      int length = 30;
+      // String to fill in blank spots in c array
+      String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      Random rand = new Random();
+      for(int i = 0; i < height; i++) {
+         for(int j = 0; j < length; j++) {
+            // compares ascii values to zero to
+            // determine if char element is blank
+            if (c[i][j] == 0) {
+               c[i][j] = characters.charAt(rand.nextInt(25));
+            
+            } else {
+               continue;
+            }    
+         }
+      }
+      for(int i = 0; i<height; i++) {
+         for(int j = 0; j<length; j++) {
+          // Prints out individaul characters
+          // places a space between characters
+            System.out.print(c[i][j] + " ");
+         }
+         System.out.println();
+      }
+      System.out.println("\n");
+   } // end of print method
+   
+   // Start of the printSolution method
+   public void printSolution(char [][] b) {
+      int height = 30;
+      int length = 30;
+      for(int i = 0; i < height; i++) {
+         for(int j = 0; j < length; j++) {
+            // compares ascii values to zero to
+            // determine if char element is blank
+            if (b[i][j] == 0) {
+               b [i][j] = '*';
+            } else {
+               continue;
+            }    
+         }
+      }
+      for(int i = 0; i<height; i++) {
+         for(int j = 0; j<length; j++) {
+           // Prints out individaul characters
+          // places a space between characters
+             System.out.print(b[i][j] + " ");
+         }
+         System.out.println();
+      }
+      System.out.println("\n");
+   } // end of printSolution method
+} // end of WordSearch Class
+   
+   
+   
+   
+
+
+ 
+
+
+
+
+
